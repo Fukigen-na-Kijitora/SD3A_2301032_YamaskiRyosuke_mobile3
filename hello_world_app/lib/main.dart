261aa45c5011f4e20026e2a6d8b59561a261524b
Yamasaki Ryosuke <2301032@s.asojuku.ac.jp>
2025-10-09 12:28:09 +0900

    pic

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 // 画像アセットリスト
-final List<Image> imageList = <Image>
-  Image.asset('assets/cat_image.png,width: 300.0,'),
-  Image.asset(
-    'assets/dog_image.png,width: 200.0,'),
-  class MyApp extends StatelessWidget {
+final List<Image> imageList = <Image>[
+  Image.asset('assets/cat_image.png', width: 300.0),
+  Image.asset('assets/dog_image.png', width: 200.0),
+];
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
@@ -23,4 +25,3 @@ final List<Image> imageList = <Image>
     );
   }
 }
-
