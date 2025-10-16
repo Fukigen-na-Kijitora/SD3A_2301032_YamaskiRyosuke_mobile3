c1ffd515acedad41377b2c5d2bf5e547ef946400
Yamasaki Ryosuke <2301032@s.asojuku.ac.jp>
2025-10-16 12:00:16 +0900

    10/16

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -6,20 +6,43 @@ void main() {
 
 // 画像アセットリスト
 final List<Image> imageList = <Image>[
-  Image.asset('assets/cat_image.png', width: 300.0),
-  Image.asset('assets/dog_image.png', width: 200.0),
+  Image.asset(
+    'assets/cat_image.png',
+    width: 300.0,
+  ),
+  Image.asset(
+    'assets/dog_image.png',
+    width: 300.0,
+  ),
 ];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sample App",
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Sample Custom Widget App')),
-        body: Center(
-          child: imageList[0],
+    return const MaterialApp(
+      title: "Image List App",
+      home: ImageListPage(),
+    );
+  }
+}
+
+class ImageListPage extends StatelessWidget {
+  const ImageListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ワンちゃん画像')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            imageList[0], // 猫の画像
+            const SizedBox(height: 20), // 画像の間にスペース
+            imageList[1], // 犬の画像
+          ],
         ),
       ),
     );
---
+++ text_input_todo_app/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+cclass MainPage extends StatelessWidget {
+  const MainPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Todo App',
+      home: TextInputWidget(),
+    );
+  }
+}
+
+//画面用の自作Widget
+class TextInputWidget extends StatefulWidget {
+  const TextInputWidget({super.key});
+  @override
+  State createState() => _TextInputWidgetState();
+}
+
+//textinputWidgetのStatekulass
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final _controller = TextEditingController();
+  List todoList = ["name"];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = '初期値が入っています';
+  }
+
+  //画面のUIを構築するbuildメソッド
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(165, 190, 215, 1.0),
+      appBar: AppBar(title: const Text('Text Input Sample')),
+      body: ListView.builder(
+        itemCount: todoList.length + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == todoList.length) {
+            // リスト一覧の最後のindexにTextFieldを作成
+            return _createTextArea();
+          } else {
+            // それ以外はTodoカードを作成
+            var title = todoList[index];
+            return _createTodoCard(title);
+          }
+        },
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+//todoカードを作成するWidget
+  Widget _createTodoCard(String title) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10.0, 
+        vertical: 5.0
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          ListTile(title: Text(title)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // 完了したときの処理
+                },
+                child: const Text('完了')
+              ),ElevatedButton(
+                onPressed: () {
+                  // 削除したときの処理
+                },
+                child: const Text('削除')
+              )
+            ],
+          )
+        ],
+      )
+    );
+  }
